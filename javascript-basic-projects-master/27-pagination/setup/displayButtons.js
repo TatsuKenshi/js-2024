d3fce4f6976a4cc1875cd0e1088f209dbce24b2b
TatsuKenshi <rockinbg@gmail.com>
2024-07-10 18:50:19 +0200

    project 27

--- javascript-basic-projects-master/27-pagination/setup/displayButtons.js
+++ javascript-basic-projects-master/27-pagination/setup/displayButtons.js
@@ -1,3 +1,12 @@
-const displayButtons = () => {}
+const displayButtons = (btnContainer, pages, activeBtnIndex) => {
+  let btns = pages.map((_, pageIndex) => {
+    return `<button class="page-btn ${
+      activeBtnIndex === pageIndex ? "active-btn" : null
+    }" data-index="${pageIndex}">${pageIndex + 1}</button>`;
+  });
+  btns.push("<button class='next-btn'>next</button>");
+  btns.unshift("<button class='prev-btn'>prev</button>");
+  btnContainer.innerHTML = btns.join("");
+};
 
-export default displayButtons
+export default displayButtons;
--- javascript-basic-projects-master/27-pagination/setup/displayFollowers.js
+++ javascript-basic-projects-master/27-pagination/setup/displayFollowers.js
@@ -1,3 +1,17 @@
-const display = () => {}
+const container = document.querySelector(".container");
 
-export default display
+const displayFollowers = (followers) => {
+  const newFollowers = followers
+    .map((person) => {
+      const { avatar_url, login, html_url } = person;
+      return `<article class="card">
+    <img src="${avatar_url}" alt="${login}" />
+    <h4>${login}</h4>
+    <a href="${html_url}" class="btn">View Profile</a>
+    </article>`;
+    })
+    .join("");
+  container.innerHTML = newFollowers;
+};
+
+export default displayFollowers;
