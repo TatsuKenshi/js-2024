d1e35fc6c5f67933c7d17d478643d800d00ede4d
TatsuKenshi <rockinbg@gmail.com>
2024-07-19 16:58:34 +0200

    29 - home & about pages html, sidebar & cart toggle func

--- javascript-basic-projects-master/29-comfy-store/starter/src/cart/toggleCart.js
+++ javascript-basic-projects-master/29-comfy-store/starter/src/cart/toggleCart.js
@@ -1,3 +1,16 @@
-import { getElement } from '../utils.js';
+import { getElement } from "../utils.js";
 
-export const openCart = () => {};
+const toggleCart = getElement(".toggle-cart");
+const cartOverlay = getElement(".cart-overlay");
+const closeCartBtn = getElement(".cart-close");
+
+toggleCart.addEventListener("click", () => {
+  cartOverlay.classList.add("show");
+});
+closeCartBtn.addEventListener("click", () => {
+  cartOverlay.classList.remove("show");
+});
+
+export const openCart = () => {
+  cartOverlay.classList.add("show");
+};
